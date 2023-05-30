42b9c0122f24c70f2e24d8f3de36ea5f882c61ba
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2023-05-30 23:54:56 +0300

    Delete LeagueOfLegendsEsports/LeagueOfLegendsEsports directory

--- LeagueOfLegendsEsports/LeagueOfLegendsEsports/src/main/java/com/example/LeagueOfLegendsEsports/repository/ChampionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.LeagueOfLegendsEsports.repository;
-
-import com.example.LeagueOfLegendsEsports.entity.Champion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChampionRepo extends JpaRepository<Champion, Long> {
-}
